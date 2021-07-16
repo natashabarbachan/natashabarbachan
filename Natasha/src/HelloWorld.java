88cc1104abd2626573db286d3dcdc191e1ddd67c
natashabarbachan <natashabarbachan@gmail.com>
2021-07-15 21:08:47 -0300

    Teste

--- Natasha/src/HelloWorld.java
+++ Natasha/src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println("Segundo Commit");
 		System.out.println("Terceiro Commit");
 		System.out.println("Prática para a disciplina de GCMS");
-		
+		// Teste
 		System.out.println("Adicionando nova linha da Prática 03");
 		System.out.println("Parte 1 - Passo 2 - Modificando em master");
 		System.out.println("Parte 1 - Passo 2");
